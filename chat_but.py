bcddd2d4af381281b11fa0dd94f813a526ac2aea
zeinab2309 <poormoosavie0@gmail.com>
2024-12-20 03:13:23 +0330

    add list for write word

--- chat_but.py
+++ chat_but.py
@@ -1,2 +1,14 @@
-import pyautogui
-print("hi")
+import pyautogui as pg
+#برای مطالعه و اطلاعات بیشتر درباره کتبخانه بالا از سایت زیر استفاده کنید
+# https://pyautogui.readthedocs.io/en/latest/
+from time import sleep
+from random import choice
+lst=["would you like to talk to me?","how are you?","123","good day","I wish you happiness"]
+pg.click(530, 999)
+pg.write("slam i,m robot \n", interval=0.1)
+for i in range(5):
+    sleep(0.1)
+    pg.click(530, 999)
+    pg.write(choice(lst), interval=0.1)
+    pg.press('enter')
+pg.hotkey('ctrl','enter')
